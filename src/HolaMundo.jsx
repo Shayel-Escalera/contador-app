bac506f426e4f26b3d67cbabaeff4a4b0ad31e33
Shayel-Escalera <shayelita1410@gmail.com>
2025-11-25 19:53:24 -0400

    cambio para produccion

---
+++ src/HolaMundo.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const HolaMundoo = () => {
+    return (
+        <div>
+            <h1>¡Hola Mundo! </h1>
+            <p>Bienvenido a mi aplicacion react</p>
+            <p>esta aplicacion esta en produccion</p>
+        </div>
+    )
+}
